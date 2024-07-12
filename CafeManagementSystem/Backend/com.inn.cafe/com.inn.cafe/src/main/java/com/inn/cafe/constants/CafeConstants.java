667c2bdebff6fb352abfbc2fc980e683eeedbfc3
PavithraMarimuthuu <pavithramv01@gmail.com>
2024-07-12 14:46:05 +0530

    commited

---
+++ CafeManagementSystem/Backend/com.inn.cafe/com.inn.cafe/src/main/java/com/inn/cafe/constants/CafeConstants.java
@@ -0,0 +1,8 @@
+package com.inn.cafe.constants;
+
+public class CafeConstants {
+	
+	public static final String SOMETHING_WENT_WRONG="something went wrong";
+    
+	public static final String IVALID_DATA= "Invalid data";
+}
